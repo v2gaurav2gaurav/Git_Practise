8cb6ae768979cdade54448e2e4706cef4aa3f16c
Gaurav <v2gaurav@gmail.com>
2021-06-23 11:36:53 +0530

    initial commit

---
+++ gaurav.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		void fun()
+		{
+			cout <<"Hello gaurav Beta: " << endl;
+		}
+};
+
+int main()
+{
+	A obj;
+	obj.fun();
+	return 0;
+}
