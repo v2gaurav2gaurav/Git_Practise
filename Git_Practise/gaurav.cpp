da7d36cac67bfef35f7c6eb0a4f2215b5b661ba7
Gaurav <v2gaurav@gmail.com>
2021-06-23 12:28:17 +0530

    commit 1

---
+++ Git_Practise/gaurav.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class B
+{
+	public:
+		void fun()
+		{
+			cout <<"Hello gaurav Beta: " << endl;
+		}
+};
+
+int main()
+{
+	B obj;
+	obj.fun();
+	return 0;
+}
